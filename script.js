2524fcd06d2a94ce89dc6a98618ea003bedc5ab8
jinofuente <jinofuente.h@gmail.com>
2023-11-11 22:25:03 -0500

    creando boton de eliminar y tambien agregando evento de eliminar

--- script.js
+++ script.js
@@ -11,12 +11,32 @@ addBtn.addEventListener("click", (e) => {
 
     const text = input.value; // cuando se pique en el botn va agarrar el valor que este dentro del imput
 
+    //INICIO DE AGREGANDO ELEMENTOS AL HTML
     const li = document.createElement("li");
     const p = document.createElement("p");
     p.textContent = text;
 
     li.appendChild(p);
+    li.appendChild(addDeleteBtn());
     ul.appendChild(li);
+    //FIN DE AGREGANDO ELEMENTOS AL HTML
 
     input.value = "";
 })
+
+// creando funcion para crear boton de eliminar y eliminar el item
+function addDeleteBtn(){
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "X";   // agregand contenido al btn
+    deleteBtn.className = "btn-delete";   // agregando stilo
+
+    deleteBtn.addEventListener('click', (e) =>{
+        //target = se refiere al boton
+        //parentElement = elimina  el elemento de arriba el li
+        const item = e.target.parentElement;
+        //removeChild = del padre elimina al hijo
+        ul.removeChild(item);
+    })
+    return addDeleteBtn;
+}
